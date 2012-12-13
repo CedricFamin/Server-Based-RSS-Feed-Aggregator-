97b50331e1b002b96c42d26b69dc2f83c973f6dd
Cedric Famin <cedric.famin@gmail.com>
2012-12-13 22:52:36 +0100

    Client WPF: Ajout de l'UserDataModel

---
+++ Client_WPF/DataModel/UserDataModel.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Client_WPF.RSSService;
+using System.ServiceModel;
+
+namespace Client_WPF.DataModel
+{
+    class UserDataModel
+    {
+        #region Common
+        static private string ConnectionString = null;
+        static private AccountClient accountClient = null;
+        private static User user { get; set; }
+
+        static public User User
+        {
+            get { return user; }
+            private set { user = value; }
+        }
+
+        static private AccountClient AccountClient
+        {
+            get
+            {
+                if (accountClient == null)
+                    accountClient = new AccountClient();
+                if (accountClient.State == CommunicationState.Closed)
+                    accountClient.Open();
+                return accountClient;
+            }
+        }
+        #endregion
+
+        public WebResult.ErrorCodeList Error { get; private set; }
+
+        #region CTor/DTor
+        public UserDataModel()
+        {
+            AccountClient.LoginCompleted += new EventHandler<LoginCompletedEventArgs>(OnEndLogin);
+            AccountClient.RegisterCompleted += new EventHandler<RegisterCompletedEventArgs>(OnEndRegister);
+        }
+
+        ~UserDataModel()
+        {
+        }
+        #endregion
+
+        #region Action
+        public void Login(string username, string password)
+        {
+            AccountClient.LoginAsync(username, password);
+        }
+
+        public void Register(string username, string password)
+        {
+            AccountClient.RegisterAsync(username, username, password);
+        }
+
+        public void Logout(string username, string password)
+        {
+            AccountClient.LogoutAsync(ConnectionString);
+            ConnectionString = null;
+        }
+
+        public void Update()
+        {
+            AccountClient.Update(ConnectionString, User);
+        }
+        #endregion
+
+        #region ResultAsync
+        private void OnEndLogin(object sender, LoginCompletedEventArgs args)
+        {
+            if (args.Error == null)
+            {
+                if (args.Result.ErrorCode == WebResult.ErrorCodeList.SUCCESS)
+                {
+                    ConnectionString = args.Result.Value.Item1.session_key;
+                    User = args.Result.Value.Item2;
+                }
+                Error = args.Result.ErrorCode;
+            }
+        }
+
+        private void OnEndRegister(object sender, RegisterCompletedEventArgs args)
+        {
+            if (args.Error == null)
+            {
+                Error = args.Result.ErrorCode;
+            }
+        }
+        #endregion
+    }
+}
--- Client_WPF/ViewModel/LoginPageViewModel.cs
+++ Client_WPF/ViewModel/LoginPageViewModel.cs
@@ -6,32 +6,18 @@ using System.Windows.Input;
 using Client_WPF.Utils;
 using Client_WPF.RSSService;
 using System.Net;
-using Client_WPF.ServiceReference;
 using System.ServiceModel.Channels;
 using System.ServiceModel;
+using Client_WPF.DataModel;
 
 namespace Client_WPF.ViewModel
 {
     public class LoginPageViewModel : BindableObject
     {
-        private User _user;
-        CookieContainer cookie;
-
-        public User User
-        {
-            get { return _user; }
-            set
-            {
-                _user = value;
-                RaisePropertyChange("User");
-            }
-        }
-
 
         #region properties
         public ICommand Login { get; private set; }
-        private AccountClient accountService { get; set; }
-        private AuthenticationServiceClient authService { get; set; }
+        private UserDataModel UserData { get; set; }
 
         private string _username;
         public string Username
@@ -54,19 +40,6 @@ namespace Client_WPF.ViewModel
                 RaisePropertyChange("Password");
             }
         }
-
-        private string _email;
-
-        public string Email
-        {
-            get { return _email; }
-            set
-            {
-                _email = value;
-                RaisePropertyChange("Email");
-            }
-        }
-
 
         private bool _register;
         public bool Register
@@ -106,65 +79,22 @@ namespace Client_WPF.ViewModel
         #region CTor
         public LoginPageViewModel()
         {
-            _username = "toto";
+            UserData = new UserDataModel();
             Login = new RelayCommand((param) => LoginBody(param as string[]));
-
-            authService = new AuthenticationServiceClient();
-            authService.Endpoint.Behaviors.Add(new CookieBehavior(""));
-
-            accountService = new AccountClient();
-            
-            accountService.LoginCompleted += new EventHandler<LoginCompletedEventArgs>(OnEndLogin);
-            accountService.RegisterCompleted += new EventHandler<RegisterCompletedEventArgs>(OnEndRegister);
             _error = 0;
             _logued = false;
-        }
-        #endregion
-
-        string sharedCookie;
-        #region ResultAsync
-        private void OnEndLogin(object sender, LoginCompletedEventArgs args)
-        {
-            if (args.Error == null)
-            {
-                if (args.Result.ErrorCode == WebResult.ErrorCodeList.SUCCESS)
-                {
-                    // Extract the cookie embedded in the received web service response
-                    // and stores it locally
-                    HttpResponseMessageProperty response = (HttpResponseMessageProperty)
-                    OperationContext.Current.IncomingMessageProperties[
-                        HttpResponseMessageProperty.Name];
-                    sharedCookie = response.Headers["Set-Cookie"];
-
-                    Logued = true;
-                    User = args.Result.Value;
-                }
-                ErrorCode = args.Result.ErrorCode;
-            }
-        }
-
-        private void OnEndRegister(object sender, RegisterCompletedEventArgs args)
-        {
-            if (args.Error == null)
-            {
-                if (args.Result.ErrorCode == WebResult.ErrorCodeList.SUCCESS)
-                {
-                    accountService.LoginAsync(Username, Password);
-                }
-                ErrorCode = args.Result.ErrorCode;
-            }
         }
         #endregion
 
         #region BodyCommand
         private void LoginBody(string[] param)
         {
-            //accountService.RegisterAsync("test", "test", "test");
             if (Register)
-                accountService.RegisterAsync(Username, Email, Password);
+                UserData.Register(Username, Password);
             else
-                accountService.LoginAsync(Username, Password);
+                UserData.Login(Username, Password);
         }
         #endregion
+
     }
 }
