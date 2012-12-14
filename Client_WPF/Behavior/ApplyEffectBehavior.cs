d68582aa3efc0f48ae0fa9019647ee8a51d1cc99
Cedric Famin <cedric.famin@gmail.com>
2012-12-14 01:23:28 +0100

    Client WPF: ApplyEffectBehavior et MenuViewModel

---
+++ Client_WPF/Behavior/ApplyEffectBehavior.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Interactivity;
+using System.Windows;
+using System.Windows.Media.Effects;
+using System.Windows.Media;
+using Client_WPF.View;
+
+namespace Client_WPF.Behavior
+{
+    class ApplyEffectBehavior : Behavior<LoginModal>
+    {
+
+        public Effect EffectToApply
+        {
+            get { return (Effect)GetValue(EffectToApplyProperty); }
+            set { SetValue(EffectToApplyProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for EffectToApply.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty EffectToApplyProperty =
+            DependencyProperty.Register("EffectToApply", typeof(Effect), typeof(ApplyEffectBehavior), null);
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.Deactivated += new EventHandler(AssociatedObject_Deactivated);
+            AssociatedObject.Initialized += new EventHandler(AssociatedObject_Initialized);
+        }
+
+        void AssociatedObject_Initialized(object sender, EventArgs e)
+        {
+            Application.Current.MainWindow.Effect = EffectToApply;
+        }
+
+        void AssociatedObject_Deactivated(object sender, EventArgs e)
+        {
+            Application.Current.MainWindow.Effect = null;
+        }
+    }
+}
---
+++ Client_WPF/ViewModel/MenuViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Client_WPF.DataModel;
+using Client_WPF.View;
+using System.Windows.Input;
+using Client_WPF.Utils;
+using System.Windows;
+
+namespace Client_WPF.ViewModel
+{
+    class MenuViewModel : BindableObject
+    {
+        #region Properties
+        private string search;
+        public string Search
+        {
+            get { return search; }
+            set
+            {
+                search = value;
+                RaisePropertyChange("Search");
+
+            }
+        }
+
+        public ICommand CloseCommand { get; private set; }
+        public ICommand ShowConnectionDialog { get; private set; }
+
+        private UserDataModel UserData { get;set; }
+
+        #endregion
+
+        #region CTor
+        public MenuViewModel()
+        {
+            UserData = new UserDataModel();
+            CloseCommand = new RelayCommand((param) => Application.Current.Shutdown());
+            ShowConnectionDialog = new RelayCommand((param) => ShowConnectionModel_IFN());
+
+            search = "Search Feeds ...";
+        }
+        #endregion
+
+        #region private methods
+        void ShowConnectionModel_IFN()
+        {
+            UserData.ShowConnexionModel_IFN();
+        }
+        #endregion
+
+    }
+}
